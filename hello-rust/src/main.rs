a3b27dc4ba51738fe38795f8298e640a3c676a5f
kei2100 <kei.arima.2100@gmail.com>
2020-10-18 20:51:56 +0900

    fix

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -2,10 +2,17 @@ use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
+    hello();
+
     let stdout = stdout();
     let message = String::from("Hello fellow Rustaceans!");
     let width = message.chars().count();
 
     let mut writer = BufWriter::new(stdout.lock());
     say(message.as_bytes(), width, &mut writer).unwrap();
+}
+
+fn hello() {
+    // ! 付きは関数呼び出しではなくマクロ呼び出し
+    println!("hello world!");
 }
