11e48274337d2e102441b9b0f85f987044dc3913
kei2100 <kei.arima.2100@gmail.com>
2020-10-25 20:47:59 +0900

    upd

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,20 +1,52 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    // thread_rng() 実行スレッドに固有で、OSによりシード値を与えられる
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    // * let mut: `mutable` variable (NOTE: default is `immutable`)
-    // * String はサイズ可変で UTF-8 なテキスト片を表す
-    // * ::new は String の関連関数 new を呼び出すことを表す (NOTE: 関連関数は他の言語におけるいわゆる静的メソッド)
-    let mut guess = String::new();
+    // println!("The secret number is: {}", secret_number);
 
-    // * & で guess の参照を渡す。Rust では参照も default immutable であるため、mut を付与して可変にする必要がある。
-    // * read_line は io::Result を返却する。io::Result は列挙型（enum）であり、Ok、Err のどちらとなる。
-    //   expect() メソッドは、Result が Err のとき、プログラムをクラッシュし引数の文字列を表示して終了する。Ok の場合は、Ok が保持する返り値を取り出して返却する。
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line!");
+    loop {
+        println!("Please input your guess. (1 to 100)");
 
-    println!("You guessed: {}", guess);
+        // * let mut: `mutable` variable (NOTE: default is `immutable`)
+        // * String はサイズ可変で UTF-8 なテキスト片を表す
+        // * ::new は String の関連関数 new を呼び出すことを表す (NOTE: 関連関数は他の言語におけるいわゆる静的メソッド)
+        let mut guess = String::new();
+
+        // * & で guess の参照を渡す。Rust では参照も default immutable であるため、mut を付与して可変にする必要がある。
+        // * read_line は io::Result を返却する。io::Result は列挙型（enum）であり、Ok、Err のどちらとなる。
+        //   expect() メソッドは、Result が Err のとき、プログラムをクラッシュし引数の文字列を表示して終了する。Ok の場合は、Ok が保持する返り値を取り出して返却する。
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        // guess はシャドーイング
+        // :u32 を付与することで、parse() が成功した場合の戻り値は u32 型として推論される?
+        // secret_number 変数も u32 型として推論される。
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        // match 式
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
