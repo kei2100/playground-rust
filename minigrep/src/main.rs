05c4aeddfb6c6fb0fa937ae707cdc569d237787a
kei2100 <kei.arima.2100@gmail.com>
2021-01-10 16:41:32 +0900

    mod

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,11 +1,32 @@
-use std::env;
+extern crate minigrep;
 
+use std::{env, process};
+use minigrep::Config;
+
+
+// Rust コミュニティの main 関数ガイドライン
+//
+// * プログラムをmain.rsとlib.rsに分け、ロジックをlib.rsに移動する。
+// * コマンドライン引数の解析ロジックが小規模な限り、main.rsに置いても良い。
+// * コマンドライン引数の解析ロジックが複雑化の様相を呈し始めたら、main.rsから抽出してlib.rsに移動する。
+//
+// この工程の後にmain関数に残る責任は以下に限定される:
+// * 引数の値でコマンドライン引数の解析ロジックを呼び出す
+// * 他のあらゆる設定を行う
+// * lib.rsのrun関数を呼び出す
+// * runがエラーを返した時に処理する
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
