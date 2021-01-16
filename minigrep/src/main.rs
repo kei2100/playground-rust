eda3d5f3e338813ff576a0b156992d200c3dc6de
kei2100 <kei.arima.2100@gmail.com>
2021-01-17 06:59:22 +0900

    use iter

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -15,9 +15,7 @@ use std::{env, process};
 // * lib.rsのrun関数を呼び出す
 // * runがエラーを返した時に処理する
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    // ベクタが参照する[String]（String のスライス）への参照を Config::new に渡している
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
